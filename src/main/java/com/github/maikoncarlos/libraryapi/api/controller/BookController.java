8f3c93ced0a4f91ae3f961b782fed0436326d322
Maikoncarlos <maikonsaturno@gmail.com>
2023-01-20 21:27:39 -0300

    mudança do mapper struct para model mapper

--- src/main/java/com/github/maikoncarlos/libraryapi/api/controller/BookController.java
+++ src/main/java/com/github/maikoncarlos/libraryapi/api/controller/BookController.java
@@ -1,13 +1,14 @@
 package com.github.maikoncarlos.libraryapi.api.controller;
 
 import com.github.maikoncarlos.libraryapi.api.dto.BookDto;
-import com.github.maikoncarlos.libraryapi.api.entity.Book;
+import com.github.maikoncarlos.libraryapi.api.entity.BookEntity;
 import com.github.maikoncarlos.libraryapi.api.exceptions.ApiErrors;
-import com.github.maikoncarlos.libraryapi.api.mapper.BookMapper;
 import com.github.maikoncarlos.libraryapi.api.service.BookService;
 import com.github.maikoncarlos.libraryapi.exception.BusinessException;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
@@ -19,29 +20,25 @@ import javax.validation.Valid;
 
 
 @RestController
-@AllArgsConstructor
-@NoArgsConstructor
 @RequestMapping("/api/books")
 public class BookController {
-
     @Autowired
     private BookService bookService;
-
     @Autowired
-    private BookMapper bookMapper;
+    private ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDto creater(@RequestBody @Valid BookDto bookDTO) {
-        Book book = bookMapper.toBook(bookDTO);
+        BookEntity book = modelMapper.map(bookDTO, BookEntity.class);
         book = bookService.save(book);
-        return bookMapper.toBookDTO(book);
+        return modelMapper.map(book, BookDto.class);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public BookDto getById(@PathVariable Long id){
        return bookService.getById(id)
-               .map(book -> bookMapper.toBookDTO(book))
+               .map(book -> modelMapper.map(book, BookDto.class))
                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
